58db21ebabc6c28ff4d3d127551ff4dc43f63d47
Benzazoua <billalben252@gmail.com>
2025-09-17 16:58:37 +0100

    Template Method Pattern

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
-import me.billal.strategy.*;
+import me.billal.template.TransferMoneyTask;
 
 public class Main {
     public static void main(String[] args) {
-        var imageStorage = new ImageStorage();
-        imageStorage.store("a", new JpgCompressor(), new BalckAndWhiteFilter());
-
-        System.out.println("--------------------------");
-
-        imageStorage.store("a", new PngCompressor(), new HIghContrast());
+        var task = new TransferMoneyTask();
+        task.execute();
     }
 }
---
+++ src/me/billal/template/AuditTrail.java
@@ -0,0 +1,7 @@
+package me.billal.template;
+
+public class AuditTrail {
+    public void record() {
+        System.out.println("Audit");
+    }
+}
