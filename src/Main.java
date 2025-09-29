f6bfe1b8a5b547ebb0ee00076a5251a7f6e422a5
Benzazoua <billalben252@gmail.com>
2025-09-29 22:46:32 +0100

    Proxy Pattern

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
-import me.billal.bridge.AdvancedRemoteControl;
-import me.billal.bridge.RemoteControl;
-import me.billal.bridge.SamsungTV;
-import me.billal.bridge.SonyTV;
+import me.billal.proxy.EbookProxy;
+import me.billal.proxy.Library;
 
 public class Main {
     public static void main(String[] args) {
-        var remoteControl = new RemoteControl(new SonyTV());
-        remoteControl.turnOn();
-        remoteControl.turnOff();
+        var library = new Library();
 
-        var remoteControl2 = new AdvancedRemoteControl(new SonyTV());
-        remoteControl2.setChannel(2);
+        String[] fileNames = { "a", "b", "c" };
+        for (var fileName : fileNames) {
+            library.add(new EbookProxy(fileName));
+        }
 
-        var remoteControl3 = new AdvancedRemoteControl(new SamsungTV());
-        remoteControl3.turnOn();
+        library.openEbook("a");
     }
 }
---
+++ src/me/billal/proxy/LoggingEbookProxy.java
@@ -0,0 +1,24 @@
+package me.billal.proxy;
+
+public class LoggingEbookProxy implements Ebook {
+  private final String fileName;
+  private RealEbook ebook;
+
+  public LoggingEbookProxy(String fileName) {
+    this.fileName = fileName;
+  }
+
+  @Override
+  public void show() {
+    if (ebook == null)
+      ebook = new RealEbook(fileName);
+
+    System.out.println("Logging");
+    ebook.show();
+  }
+
+  @Override
+  public String getFileName() {
+    return fileName;
+  }
+}
